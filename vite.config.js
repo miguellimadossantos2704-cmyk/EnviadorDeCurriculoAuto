8fd9479b68ee3bb904c83e46b1500417b3c83e8d
miguellimadossantos2704-cmyk <miguellimadossantos2704@gmail.com>
2026-03-05 10:39:41 -0300

    Refactor vite.config.js for cleaner formatting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Adicionando a base para o GitHub Pages reconhecer a pasta do repositório
-  base: '/EnviadorDeCurriculoAuto/',
+  base: '/EnviadorDeCurriculoAuto/', 
 })
