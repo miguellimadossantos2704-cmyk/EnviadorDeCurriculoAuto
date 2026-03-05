f5c06e0e6218a62ec595f427cd0b521528983dd7
miguellimadossantos2704-cmyk <miguellimadossantos2704@gmail.com>
2026-03-05 10:25:32 -0300

    Set base path for GitHub Pages
    Add base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Adicionando a base para o GitHub Pages reconhecer a pasta do repositório
+  base: '/EnviadorDeCurriculoAuto/',
 })
